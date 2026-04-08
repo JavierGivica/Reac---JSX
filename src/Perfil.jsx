b9a4c57198b5c47d893fcaa23258e035277bed2e
JavierGivica <xgivica@socialh4ck.com>
2026-04-08 10:54:44 +0200

    Ejercicio 1 — Exploracion de JSX

---
+++ src/Perfil.jsx
@@ -0,0 +1,24 @@
+const Perfil = () => {
+
+  const nombre = "Javier";
+
+  return (
+
+    <>
+
+      <h2>{nombre}</h2>
+
+
+      <p>Carrera: Desarrollo de Aplicaciones Web (DAW), 2do año de estudio</p>
+
+
+      <ul>
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>CSS</li>
+      </ul>
+    </>
+  );
+};
+
+export default Perfil;
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Perfil from './Perfil.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Perfil />
+  </React.StrictMode>,
+)
